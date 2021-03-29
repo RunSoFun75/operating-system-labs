81954f8f27d593101b0758e215e199aef228c4f3
Andrey <andrey.bazarov75@gmail.com>
2021-03-29 19:36:49 +0700

    add command completion code

---
+++ command completion code/cmake-build-debug/CMakeFiles/command_completion_code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/command_completion_code.dir/main.c.o"
+  "command_completion_code"
+  "command_completion_code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/command_completion_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ command completion code/cmake-build-debug/CMakeFiles/command_completion_code.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for command_completion_code.
+# This may be replaced when dependencies are built.
