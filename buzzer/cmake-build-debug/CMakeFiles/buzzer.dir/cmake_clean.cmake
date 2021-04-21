bab6a5a0af9a524c5b54010cf5b14e80b78137f2
Andrey <andrey.bazarov75@gmail.com>
2021-04-21 19:52:26 +0700

    add buzzer

---
+++ buzzer/cmake-build-debug/CMakeFiles/buzzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buzzer.dir/main.c.o"
+  "buzzer"
+  "buzzer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/buzzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ buzzer/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#define BEL '\007'
+
+int sounds = 0;
+int is_end = 0;
+
+void sound() {
+    sounds++;
+}
+
+void end() {
+    is_end++;
+}
+
+int main(int argc, char *argv[]) {
+    struct sigaction sigct1, sigct2;
+    sigct1.sa_handler = sound;
+    sigct2.sa_handler = end;
+    sigaction(SIGINT, &sigct1, NULL);
+    sigaction(SIGQUIT, &sigct2, NULL);
+    while (1) {
+        pause();
+        printf("%c\n", BEL);
+        if (is_end) {
+            printf("amount of sounds: %d\n", sounds);
+            exit(0);
+        }
+    }
+}
