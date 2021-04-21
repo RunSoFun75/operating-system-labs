a25036a8663730d106de44b8489684d424e80e59
Andrey <andrey.bazarov75@gmail.com>
2021-04-21 19:27:55 +0700

    fix answer without entering a new line v1.0

--- answer without entering a new line/main.c
+++ answer without entering a new line/main.c
@@ -29,7 +29,10 @@ int ask(int desc) {
     } else if (write(desc, "\n", 1) == -1) {
         perror("write failed");
         return -1;
-    } else if (write(desc, inputSymbols, 1) == -1) {
+    } else if (write(desc, "Good Answer!", 12) == -1) {
+        perror("write failed");
+        return -1;
+    } else if (write(desc, "\n", 1) == -1) {
         perror("write failed");
         return -1;
     }
