9fa822033b94ccb32ff57671e8405bf956368b15
Andrey <andrey.bazarov75@gmail.com>
2021-04-25 17:33:57 +0700

    add counting empty lines in a file

---
+++ counting empty lines in a file/cmake-build-debug/CMakeFiles/counting_empty_lines_in_a_file.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/counting_empty_lines_in_a_file.dir/main.c.o"
+  "counting_empty_lines_in_a_file"
+  "counting_empty_lines_in_a_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/counting_empty_lines_in_a_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ counting empty lines in a file/cmake-build-debug/CMakeFiles/counting_empty_lines_in_a_file.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for counting_empty_lines_in_a_file.
+# This may be replaced when dependencies are built.
