f12eec817da42c27a56109b8579abbbcf56b3025
Andrey <andrey.bazarov75@gmail.com>
2021-04-28 23:02:57 +0700

    add unix domain socket communication

---
+++ unix domain socket communication/cmake-build-debug/CMakeFiles/unix_domain_socket_communication.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unix_domain_socket_communication.dir/main.c.o"
+  "unix_domain_socket_communication"
+  "unix_domain_socket_communication.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unix_domain_socket_communication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ unix domain socket communication/cmake-build-debug/CMakeFiles/unix_domain_socket_communication.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for unix_domain_socket_communication.
+# This may be replaced when dependencies are built.
