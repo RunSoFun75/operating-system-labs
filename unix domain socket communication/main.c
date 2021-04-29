ac272aabbb2a253d42c040e7b4d622fb84f64ee1
Andrey <andrey.bazarov75@gmail.com>
2021-04-30 04:03:45 +0700

    add unix domain socket communication v1.0

--- unix domain socket communication/main.c
+++ unix domain socket communication/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <ctype.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define MSGSZ 128
+
+/*
+ * This lab needs to run two compiled files main (receive), send
+ * processes are stopped by a signal CTRL + C
+ */
+
+struct message_buf {
+    long mtype;
+    char mtext[MSGSZ];
+} message_buf;
+
+int main(int argc, char *argv[]) {
+    int msg_fd = msgget((int) getuid(), 0);
+
+    while (1) {
+        if (msgrcv(msg_fd, &message_buf, MSGSZ, 1L, 0) == -1) {
+            perror("can't recv");
+            exit(3);
+        }
+        for (int i = 0; message_buf.mtext[i] != '\0'; ++ i) {
+            printf("%c", toupper(message_buf.mtext[i]));
+        }
+    }
 }
---
+++ unix domain socket communication/send.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+
+#define MSGSZ 128
+
+struct message_buf {
+    long mtype;
+    char mtext[MSGSZ];
+} message_buf = {1L, "\0"};
+
+int main(int argc, char *argv[]) {
+    int msg_fd;
+    if ((msg_fd = msgget((int) getuid(), IPC_CREAT | 0640)) == -1) {
+        perror("can't msgget");
+        exit(1);
+    }
+
+    while (fgets(message_buf.mtext, MSGSZ, stdin) != NULL) {
+        if (msgsnd(msg_fd, &message_buf, MSGSZ, 0) == -1) {
+            perror("can't send");
+            exit(3);
+        }
+    }
+    printf("sfgdfgfdg");
+    return 0;
+}
