3924ec7a7de029e6b4f40023ce178e5ea51eb0a3
Andrey <andrey.bazarov75@gmail.com>
2021-05-15 03:23:48 +0700

    remove unnecessary sigprocmask()

--- creating two processes/main.c
+++ creating two processes/main.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
 #include <wait.h>
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
-    pid_t pid, ret;
-    sigset_t newset;
-    sigfillset (&newset);
-    sigprocmask(SIG_BLOCK, &newset, 0);
-    int status = 0;
+    pid_t pid;
     if (argc < 2) {
         printf("pass file as argument, please!\n");
         return 0;
     }
     if ((pid = fork()) == 0) {
         sleep(20);
-        execl("/bin/cat", "cat", argv[1], (char *) 0);
+        execl("/bin/cat", "cat", argv[1], 0);
     }
     else {
         printf("parent: waiting for child: %ld\n", pid);
     }
 
-    int ret = 0;
+    int ret = 0, status = 0;
     while (ret != -1) {
         ret = wait(&status); // wait child
     }
-
-    printf("parent: wait's return value: %ld,", ret);
     printf("child's status: %d\n", WEXITSTATUS(status));
     exit(0);
 }
